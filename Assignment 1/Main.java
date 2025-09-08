812bd4fcbb1754954bca76b4e14bacd565fbc5d5
Yabikibira <Yabikibira.asemie1@marist.edu>
2025-09-08 20:38:33 +0300

    Create Main.java

---
+++ Assignment 1/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class App 1 {
+   public static void main(String[] args) throws Exception{
+       Scanner sc=new Scanner(System.in);
+       //ask the user for thier age :3
+       System.out.println("How old are you ?");
+       //we store the users age in input >.<
+       String input = sc.nextLine();
+       //WE FINALLY PRINT IT OUTTT!!:p
+       System.out.println("Cool you are "+ input +" years old!");
+
+       //needa add this number cause melissa said so <3!!
+       int num1 =10;
+       int num2 = 37;
+       // pretty simple i prrint out num1 and num2 two by simply adding tem since java alr knows that they are numbers and needs to be added unlike python T-T
+       System.out.println(num1 + num2);
+      // i leanrt to add and print out two different data types in one line:D
+   }
+
+
+
+   
+    
+}
+
